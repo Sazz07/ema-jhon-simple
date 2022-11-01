6ae29457e5dc6e185ad8fc39d2d55aebefc11c5c
Sazz07 <sazzad.hossain882@gmail.com>
2022-11-02 00:41:00 +0600

    Complete ema jhob e-commerce site

---
+++ src/components/Shipping/Shipping.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Shipping = () => {
+    return (
+        <div>
+            <h3>Shipping</h3>
+            <p>Delivery on the way!!</p>
+            <p><small>Coming soon...</small></p>
+        </div>
+    );
+};
+
+export default Shipping;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDg3mAfnlckRkfr4Ft57SIbko426WmNhgM",
+  authDomain: "ema-jhon-simple-201fd.firebaseapp.com",
+  projectId: "ema-jhon-simple-201fd",
+  storageBucket: "ema-jhon-simple-201fd.appspot.com",
+  messagingSenderId: "984641809520",
+  appId: "1:984641809520:web:8cdce58b9dcebf267598b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
